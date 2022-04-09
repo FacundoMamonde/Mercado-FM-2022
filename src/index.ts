ec39804574c0fcc93a2585e5581da93d2c6fef21
FacundoMamonde <92485473+FacundoMamonde@users.noreply.github.com>
2022-04-08 23:32:14 -0300

    a5dkfs

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+function comprobarNumero(n: number): boolean {
+  if (isNaN(n) || n < 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function agregarItem() {
+  let nombreProducto = prompt("Ingrese el nombre del producto:");
+  while (nombreProducto === null) {
+    nombreProducto = prompt("Error: Ingrese el nombre del producto:");
+  }
+
+  let precioProducto = Number(prompt("Ingrese el precio del producto:"));
+  while (comprobarNumero(precioProducto) === false) {
+    precioProducto = Number(prompt("Error: Ingrese el precio del producto:"));
+  }
+
+  let stockProducto = Number(prompt("Ingrese el stock del producto:"));
+  while (comprobarNumero(stockProducto) === false) {
+    precioProducto = Number(prompt("Error: Ingrese el stock del producto:"));
+  }
+
+  console.log(
+    "Se añadio producto: " +
+      nombreProducto +
+      ", precio: $" +
+      precioProducto +
+      ", stock: " +
+      stockProducto +
+      " unidades"
+  );
+}
+
+let botonAgregar = document.getElementById("button-add-item");
+botonAgregar?.addEventListener("click", agregarItem);
